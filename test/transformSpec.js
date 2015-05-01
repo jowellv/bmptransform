11aea6868269f987cbc978f9f83b6c12071ed5a2
Jowell Villasin <jowell.villasin@gmail.com>
2015-05-01 13:48:36 -0700

    pixel stuff

--- test/transformSpec.js
+++ test/transformSpec.js
@@ -5,7 +5,9 @@ var transform = require('../transform');
 describe('transforms bmp files', function(){
 
   it('should read the file header correctly', function() {
-    expect(transform.readBmp('./tru256.bmp').header).to.eql('424d');
-    expect(transform.readBmp('./tru256.bmp').fileSize).to.eql('49206');
+    // expect(transform.readBmp('./tru256.bmp').header).to.eql('424d');
+    expect(transform.readBmp('./bitmap1.bmp').header).to.eql('424d');
+
+    // expect(transform.readBmp('./tru256.bmp').fileSize).to.eql('49206');
   });
 });
--- transform.js
+++ transform.js
@@ -12,13 +12,25 @@ function readBmp (filename) {
   bmpFile.width = file.readUInt32LE(18).toString(10);
   bmpFile.height = file.readUInt32LE(22).toString(10);
   bmpFile.bpp = file.readUInt32LE(28).toString(10);
+  console.log("bpp: " + bmpFile.bpp);
+  var pixelStart = +file.readUInt32LE(10).toString(10);
+  console.log("pixelStart : " + pixelStart);
+  console.log("fileLength: " + file.length);
+  // bmpFile.px = file.readUInt32LE(pixelStart).toString(16);
+  //
+  // bmpFile.px2 = file.readUInt32LE(pixelStart+4).toString(16);
+  // bmpFile.px3 = file.readUInt32LE(pixelStart+8).toString(16);
+  // bmpFile.px4 = file.readUInt32LE(pixelStart+12).toString(16);
 
-  var pixelStart = file.readUInt32LE(10).toString(10);
-
-  bmpFile.px1 = file.readUInt8LE(pixelStart).toString(16);
-  bmpFile.px2 = file.readUInt8LE(pixelStart+1).toString(16);
-  bmpFile.px3 = file.readUInt8LE(pixelStart+2).toString(16);
-
+  console.log(bmpFile.px);
+  console.log(bmpFile.px2);
+  // bmpFile.px2 = file.readUInt8(pixelStart+1).toString(16);
+  // bmpFile.px3 = file.readUInt8(pixelStart+2).toString(16);
+  //
+  // console.log(bmpFile.bpp + "bpp");
+  // console.log('0: ' + bmpFile.px1);
+  // console.log('1: ' + bmpFile.px2);
+  // console.log('2: ' + bmpFile.px3);
   return bmpFile;
 }
 
