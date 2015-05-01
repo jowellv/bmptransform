14ea8cffed188f5bed8dce26dbd97b6b427d8001
Lamson Le <ll217@u.washington.edu>
2015-05-01 12:11:01 -0700

    add width x heigth function

--- test/transformSpec.js
+++ test/transformSpec.js
@@ -7,4 +7,13 @@ describe('transforms bmp files', function(){
   it('should bmp file header correctly', function() {
     expect(transform.readBmp('./tru256.bmp')).to.eql('424d');
   });
+
+  it('should read bmp file size', function() {
+    expect(transform.readSize('./tru256.bmp')).to.eql('49206');
+  });
+
+  it('should read Width x Heigth', function() {
+    expect(transform.readImgWH('./tru256.bmp')).to.eql('256x64');
+  });
+
 });
--- transform.js
+++ transform.js
@@ -1,12 +1,15 @@
 'use strict';
 
 var fs = require('fs');
+var img = './tru256.bmp';
+var img2 = './tweety78.bmp';
 
 
 function readBmp (filename) {
   var file = fs.readFileSync(filename);
   console.log(file.readUInt16BE(0).toString(16));
   return file.readUInt16BE(0).toString(16);
+
   // fs.readFile(filename, function(err, buf) {
   //   if(err) throw err;
   //   var bmpFile = {};
@@ -19,6 +22,26 @@ function readBmp (filename) {
   // });
 }
 
+
+function readSize (filename) {
+  var file = fs.readFileSync(filename);
+  var size = file.readUInt32LE(2).toString(10);
+  console.log('28: size: ' + size);
+  return size;
+}
+
+function readImgWH (filename) {
+  var file = fs.readFileSync(filename);
+  var imgW = file.readUInt32LE(18).toString(10);
+  var imgH = file.readUInt32LE(22).toString(10);
+  console.log('Image WxH: ' + imgW + 'x' + imgH);
+  return imgW + 'x' + imgH;
+}
+
+readImgWH(img2);
+
 module.exports = {
-  readBmp: readBmp
+  readBmp: readBmp,
+  readSize: readSize,
+  readImgWH: readImgWH
 };
