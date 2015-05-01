b4d3aae9d0091b69d9784a07ac0c4e119e745dd4
Lamson Le <ll217@u.washington.edu>
2015-05-01 14:49:48 -0700

    add test

--- test/transformSpec.js
+++ test/transformSpec.js
@@ -4,11 +4,20 @@ var transform = require('../transform');
 
 describe('transforms bmp files', function(){
 
-  it('should read the file header correctly', function() {
+  it('read file header 424d', function() {
     expect(transform.readBmp('./bitmap1.bmp').header).to.eql('424d');
-    // expect(transform.readBmp('./bitmap1.bmp').fileSize).to.eql('11078');
-    // expect(transform.readBmp('./bitmap1.bmp').width).to.eql('100');
-    // expect(transform.readBmp('./bitmap1.bmp').height).to.eql('100');
+  });
+  it('read file size 11078', function() {
+    expect(transform.readBmp('./bitmap1.bmp').fileSize).to.eql('11078');
+  });
+  it('read picture width 100px', function() {
+    expect(transform.readBmp('./bitmap1.bmp').width).to.eql('100');
+  });
+  it('read picture height 100px', function() {
+    expect(transform.readBmp('./bitmap1.bmp').height).to.eql('100');
+  });
+  it('read picture bits per pixel of 8', function() {
+    expect(transform.readBmp('./bitmap1.bmp').bpp).to.eql('8');
   });
 
 
--- transform.js
+++ transform.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 var img = './tru256.bmp';
 var img2 = './tweety78.bmp';
-
+var pixels = [];
 
 function readBmp (filename) {
   var file = fs.readFileSync(filename);
@@ -14,13 +14,19 @@ function readBmp (filename) {
   bmpFile.width = file.readUInt32LE(18).toString(10);
   bmpFile.height = file.readUInt32LE(22).toString(10);
   bmpFile.bpp = file.readUInt32LE(28).toString(10);
+  var palette = file.readUInt32LE(46).toString(10);
 
   var pixelStart = +file.readUInt32LE(10).toString(10);
 
-  bmpFile.px1 = file.readUInt8(pixelStart+8).toString(16);
-  bmpFile.px2 = file.readUInt8(pixelStart+8+1).toString(16);
-  bmpFile.px3 = file.readUInt8(pixelStart+8+2).toString(16);
+  bmpFile.px1 = file.readUInt8(pixelStart);
+  bmpFile.px2 = file.readUInt8(pixelStart+1);
+  bmpFile.px3 = file.readUInt8(pixelStart+2);
+  for(var i = 0; i < 20; i++) {
+    pixels[i] = file.readUInt8(pixelStart+i+100);
+  }
+  console.log(pixels);
   console.log('pixstart: ' + pixelStart);
+  console.log('palette: ' + palette);
   console.log('R: ' + bmpFile.px1);
   console.log('G: ' + bmpFile.px2);
   console.log('B: ' + bmpFile.px3);
