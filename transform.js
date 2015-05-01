51191d51b9f51258dbd97ab638d5d4dbeb81637f
Jowell Villasin <jowell.villasin@gmail.com>
2015-05-01 16:48:41 -0700

    add transform stuff

--- transform.js
+++ transform.js
@@ -1,9 +1,6 @@
 'use strict';
 
 var fs = require('fs');
-var img = './tru256.bmp';
-var img2 = './tweety78.bmp';
-var pixels = [];
 
 function readBmp (filename) {
   var file = fs.readFileSync(filename);
@@ -16,36 +13,45 @@ function readBmp (filename) {
   bmpFile.width = file.readUInt32LE(18);
   bmpFile.height = file.readUInt32LE(22);
   bmpFile.bpp = file.readUInt32LE(28);
-  var startOfcp = 54;
-  console.log("test: "  + file.readUInt32LE(54 + 4*28).toString(16));
-  console.log("test: "  + file.readUInt32LE(54 + 4*15).toString(16));
-
-  console.log("bpp: " + bmpFile.bpp);
-
   var pixelStart = +file.readUInt32LE(10);
 
-  console.log("pixelStart : " + pixelStart);
-  console.log("fileLength: " + file.length);
-  console.log("width: " + bmpFile.width);
-  console.log("height: " + bmpFile.height);
+  var startOfcp = 14 + file.readUInt32LE(14);
+  var sizeOfcp = file.readUInt32LE(46);
+  // console.log("start of pix" + file.readUInt8(pixelStart));
+  // console.log("start of cp: " + file.readUInt32LE(startOfcp + 8).toString(16));
+  // console.log(file.readUInt32LE(startOfcp + 4 * 28).toString(16));
 
-  console.log('pixstart: ' + pixelStart);
-  console.log('colorIndex: ' + bmpFile.px1);
-  // bmpFile.colorTable = [];
-  // for(var i = pixelStart; i <= file.length; i++) {
-  //   bmpFile.colorTable.push(255 -file.readUInt8(i));
-  // }
-
+  bmpFile.colorPal = [];
+  for(var i = 0; i < sizeOfcp; i++) {
+    bmpFile.colorPal.push(file.readUInt32LE(startOfcp + 4 * i));
+  }
 
   return bmpFile;
 }
 
-function processBmp (fileObject) {
-  // body...
+function transformBmp (fileObject) {
+  fileObject.colorPal.forEach(function(colorRow, i, arr) {
+    arr[i] = 16777215 - colorRow;
+  });
+
+  return fileObject;
+}
+
+function writeBmp (fileObject, filename) {
+  var file = fs.readFileSync(filename);
+  var startOfcp = 14 + file.readUInt32LE(14);
+
+  fileObject.colorPal.forEach(function(colorRow, i, arr) {
+    file.writeUInt32LE(colorRow, startOfcp + i);
+  });
+  fs.writeFileSync('newbmp.bmp', file);
+  var outFile = fs.readFileSync('./newbmp.bmp');
+  return outFile.readUInt16BE(0).toString(16);
 }
 
 
-
 module.exports = {
-  readBmp: readBmp
+  readBmp: readBmp,
+  transformBmp: transformBmp,
+  writeBmp: writeBmp
 };
