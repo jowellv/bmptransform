bd56d97374b487eee22658fb6ad9f5c9752face9
Jowell Villasin <jowell.villasin@gmail.com>
2015-05-01 11:33:29 -0700

    add file size to obj

--- test/transformSpec.js
+++ test/transformSpec.js
@@ -4,7 +4,8 @@ var transform = require('../transform');
 
 describe('transforms bmp files', function(){
 
-  it('should bmp file header correctly', function() {
-    expect(transform.readBmp('./tru256.bmp')).to.eql('424d');
+  it('should read the file header correctly', function() {
+    expect(transform.readBmp('./tru256.bmp').header).to.eql('424d');
+    expect(transform.readBmp('./tru256.bmp').fileSize).to.eql('49206');
   });
 });
--- transform.js
+++ transform.js
@@ -5,18 +5,10 @@ var fs = require('fs');
 
 function readBmp (filename) {
   var file = fs.readFileSync(filename);
-  console.log(file.readUInt16BE(0).toString(16));
-  return file.readUInt16BE(0).toString(16);
-  // fs.readFile(filename, function(err, buf) {
-  //   if(err) throw err;
-  //   var bmpFile = {};
-  //   var header = function fileDone(buf) {
-  //     return buf.readUInt16BE(0).toString(16);
-  //   };
-  //
-  //
-  //   return header;
-  // });
+  var bmpFile = {};
+  bmpFile.header = file.readUInt16BE(0).toString(16);
+  bmpFile.fileSize = file.readUInt32LE(2).toString(10);
+  return bmpFile;
 }
 
 module.exports = {
