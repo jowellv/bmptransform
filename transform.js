3169f2920dc16077769a0c81d8dd20e136ea436f
Jowell Villasin <jowell.villasin@gmail.com>
2015-05-01 14:46:41 -0700

    read color table

--- transform.js
+++ transform.js
@@ -8,29 +8,35 @@ var img2 = './tweety78.bmp';
 function readBmp (filename) {
   var file = fs.readFileSync(filename);
   var bmpFile = {};
+
+
   // header is 14 bytes long
   bmpFile.header = file.readUInt16BE(0).toString(16);
-  bmpFile.fileSize = file.readUInt32LE(2).toString(10);
-  bmpFile.width = file.readUInt32LE(18).toString(10);
-  bmpFile.height = file.readUInt32LE(22).toString(10);
-  bmpFile.bpp = file.readUInt32LE(28).toString(10);
+  bmpFile.fileSize = file.readUInt32LE(2);
+  bmpFile.width = file.readUInt32LE(18);
+  bmpFile.height = file.readUInt32LE(22);
+  bmpFile.bpp = file.readUInt32LE(28);
+  var startOfcp = 54;
+  console.log("test: "  + file.readUInt32LE(54 + 4*28).toString(16));
+  console.log("test: "  + file.readUInt32LE(54 + 4*15).toString(16));
+
+
   console.log("bpp: " + bmpFile.bpp);
-  var pixelStart = +file.readUInt32LE(10).toString(10);
+  var pixelStart = +file.readUInt32LE(10);
   console.log("pixelStart : " + pixelStart);
   console.log("fileLength: " + file.length);
-  // bmpFile.px = file.readUInt32LE(pixelStart).toString(16);
-  //
-  // bmpFile.px2 = file.readUInt32LE(pixelStart+4).toString(16);
-  // bmpFile.px3 = file.readUInt32LE(pixelStart+8).toString(16);
-  // bmpFile.px4 = file.readUInt32LE(pixelStart+12).toString(16);
+  console.log("width: " + bmpFile.width);
+  console.log("height: " + bmpFile.height);
 
-  bmpFile.px1 = file.readUInt8(pixelStart).toString(16);
-  bmpFile.px2 = file.readUInt8(pixelStart+1).toString(16);
-  bmpFile.px3 = file.readUInt8(pixelStart+2).toString(16);
+  bmpFile.px1 = file.readUInt8(pixelStart+13);
   console.log('pixstart: ' + pixelStart);
-  console.log('R: ' + bmpFile.px1);
-  console.log('G: ' + bmpFile.px2);
-  console.log('B: ' + bmpFile.px3);
+  console.log('colorIndex: ' + bmpFile.px1);
+  // bmpFile.colorTable = [];
+  // for(var i = pixelStart; i <= file.length; i++) {
+  //   bmpFile.colorTable.push(255 -file.readUInt8(i));
+  // }
+
+
   return bmpFile;
 }
 
