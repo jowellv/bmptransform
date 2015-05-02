a328b64da9f48007838c6fea2557951d69730898
Lamson Le <ll217@u.washington.edu>
2015-05-02 11:13:31 -0700

    buffer error

--- transform.js
+++ transform.js
@@ -21,6 +21,10 @@ function readBmp (filename) {
   // console.log("start of cp: " + file.readUInt32LE(startOfcp + 8).toString(16));
   // console.log(file.readUInt32LE(startOfcp + 4 * 28).toString(16));
 
+  // console.log('pixelStart: ' + pixelStart);
+  // console.log('startOfcp: ' + startOfcp);
+  // console.log('sizeOfcp: ' + sizeOfcp);
+
   bmpFile.colorPal = [];
   for(var i = 0; i < sizeOfcp; i++) {
     bmpFile.colorPal.push(file.readUInt32LE(startOfcp + 4 * i));
@@ -34,27 +38,35 @@ function readBmp24 (filename) {
   var file = fs.readFileSync(filename);
   var bmpFile = {};
 
-  // header is 14 bytes long
   bmpFile.header = file.readUInt16BE(0).toString(16);
   bmpFile.fileSize = file.readUInt32LE(2);
   bmpFile.width = file.readUInt32LE(18);
   bmpFile.height = file.readUInt32LE(22);
   bmpFile.bpp = file.readUInt32LE(28);
+  bmpFile.pixelLength = file.readUInt32LE(34);
 
   var pixelStart = +file.readUInt32LE(10);
 
   // console.log("bpp: " + bmpFile.bpp);
-  // console.log("pixelStart : " + pixelStart);
+  console.log("pixelStart : " + pixelStart);
   // console.log("fileLength: " + file.length);
   // console.log("width: " + bmpFile.width);
   // console.log("height: " + bmpFile.height);
+  console.log('pixelLength : ' + bmpFile.pixelLength);
 
-  var pixels = [];
-  for(var i = 0; i < 50; i++) {
-    pixels[i] = file.readUInt8(54+i*3);
+  //Access beyond buffer error, can't save 10000 pixels over 30000 bits
+  bmpFile.pixels = [];
+  for( var i = 0 ; i <= 10000 ; i++) {
+    bmpFile.pixels.push(file.readUInt8(54 + i * 3));
   }
-  // console.log(pixels);
+
+  console.log('pix1000 : ' + bmpFile.pixels[1000]);
+  console.log('pixels length: ' + bmpFile.pixels.length);
   return bmpFile;
+}
+
+function transformBmp24 (fileObject) {
+
 }
 
 function transformBmp (fileObject) {
